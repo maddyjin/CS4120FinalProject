906e1a3540631c6688812cf8d983e6a09f8521f5
elijahsandler <sandler.e@northeastern.edu>
2026-04-06 16:49:54 -0400

    i do not want this anymore

---
+++ scripts/train_w2v.py
@@ -0,0 +1,17 @@
+from gensim.models import Word2Vec
+from nltk.tokenize import word_tokenize
+from collections import Counter
+from nltk.corpus import stopwords
+import string
+from glob import glob
+
+def clean_file(topic, language):
+
+    path = f'data/{topic}/*.txt'
+    paths = glob(path)
+
+    for p in paths:
+        if p.lowe
+            path = 
+
+    pass
