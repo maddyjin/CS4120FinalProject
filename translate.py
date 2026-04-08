b5b0dfd0e8f3a902beaccdc97cb2bcc736cdd109
elijahsandler <sandler.e@northeastern.edu>
2026-04-08 14:47:47 -0400

    translatuion

---
+++ translate.py
@@ -0,0 +1,29 @@
+from googletrans import Translator
+import glob
+import asyncio
+
+lang = 'spanish'
+fnames = glob.glob(f'data/*/*{lang}*.txt', recursive=True)
+
+txt = ''
+
+for file in fnames:
+    with open(file, 'rb') as f:
+        txt += f.read().decode().strip()
+
+txt = txt.replace("\r", "").replace('\n', '')
+txt
+sample = txt[:500]
+
+async def translate_text(sample, src):
+    print('>', sample)
+    async with Translator() as translator:
+        result = await translator.translate(sample, src=src, dest='en')
+        if result.text == sample:
+            raise Exception('Translation failed.')    
+        
+    return result.text
+        
+if __name__ == '__main__':
+    translated = asyncio.run(translate_text(sample, 'zh-cn'))
+    print(translated)
